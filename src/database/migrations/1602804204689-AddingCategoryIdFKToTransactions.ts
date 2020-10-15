53dad6620ec2b7be9a898707710989d66685583f
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-10-15 20:32:47 -0300

    :card_file_box: Adding foreign key to link transactions table to categories

---
+++ src/database/migrations/1602804204689-AddingCategoryIdFKToTransactions.ts
@@ -0,0 +1,35 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddingCategoryIdFKToTransactions1602804204689
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionsCategory',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropForeignKey('transactions', 'TransactionsCategory');
+  }
+}
