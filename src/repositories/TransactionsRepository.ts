f75fd0eda284a52c8565a7687ce5384f22299a14
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-11-06 22:58:41 -0200

    :construction: Adding getBalance functionality

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,8 +10,27 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
+  public async getBalance(transactions?: Array<Transaction>): Promise<Balance> {
+    const transactionsArray = transactions || (await this.find());
+    const reducer = (acc: number, cur: number): number => acc + cur;
+
+    const income = transactionsArray
+      .filter(transaction => transaction.type === 'income')
+      .map(incomeItem => Number(incomeItem.value))
+      .reduce(reducer, 0);
+
+    const outcome = transactionsArray
+      .filter(transaction => transaction.type === 'outcome')
+      .map(outcomeItem => Number(outcomeItem.value))
+      .reduce(reducer, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 }
 
