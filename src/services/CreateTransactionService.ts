664d1917a04d3a838f288be7deb29835cd4fe9ba
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-11-06 23:35:09 -0200

    :construction: Adding balance validation on transaction create

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,8 @@
-import { getRepository } from 'typeorm';
+import { getRepository, getCustomRepository } from 'typeorm';
 
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 import Category from '../models/Category';
 import Transaction from '../models/Transaction';
@@ -12,6 +14,11 @@ interface Request {
   category: string;
 }
 
+interface ValidationDTO {
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   public async execute({
     title,
@@ -19,8 +26,10 @@ class CreateTransactionService {
     type,
     category,
   }: Request): Promise<Transaction> {
-    const transactionsRepository = getRepository(Transaction);
+    const transactionsRepository = getCustomRepository(TransactionRepository);
     const categoriesRepository = getRepository(Category);
+
+    await this.validateTransaction({ value, type }, transactionsRepository);
 
     let categoryData = await categoriesRepository.findOne({
       where: {
@@ -46,6 +55,21 @@ class CreateTransactionService {
     await transactionsRepository.save(transaction);
 
     return transaction;
+  }
+
+  private async validateTransaction(
+    { value, type }: ValidationDTO,
+    repository: TransactionRepository,
+  ): Promise<void> {
+    if (type === 'income') {
+      return;
+    }
+
+    const balance = await repository.getBalance();
+
+    if (balance.total - value < 0) {
+      throw new AppError('Invalid balance', 400);
+    }
   }
 }
 
