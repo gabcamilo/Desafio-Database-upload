305c88028521b1a070be92e389cb57bf61a15ed9
Gabriela Carvalho Camilo <gabccamilo.dev@gmail.com>
2020-11-06 22:54:34 -0200

    :construction: Adding create transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,51 @@
+import { getRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
 
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getRepository(Transaction);
+    const categoriesRepository = getRepository(Category);
+
+    let categoryData = await categoriesRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!categoryData) {
+      categoryData = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(categoryData);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryData.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
